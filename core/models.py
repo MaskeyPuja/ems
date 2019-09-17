c9fb255b86cdbc4987ba699b2b23acd02f50d822
MaskeyPuja <MaskeyPuja@github.com>
2019-09-17 12:56:41 +0545

    Profile model created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+	USER = (
+		('C', 'Coordinator'),
+		('T', 'Tasker'),
+	)
+	user = models.CharField(max_length=2, choices=USER)
+	name = models.CharField(max_length=50)
+	address = models.CharField(max_length=150)
+
+	def __str__(self):
+		return self.name
